044a1bd4014a7b39579106b7aeeb07c3430502c9
BoxinCao <ngxmex@gmail.com>
2025-05-23 11:55:06 -0400

    Apply Styling to the Photo Component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,22 +1,23 @@
 import "../styles/PhotoListItem.scss";
 
 
-const PhotoListItem = ({ photo }) => {
-  const { imageSource, username, profile, location } = photo;
-
+const PhotoListItem = ({ imageSource, username, profile, location }) => {
   return (
-    <div className="photo-list-item">
-      <img src={imageSource} alt={`${username}'s photo`} className="photo" />
-      <div className="photo-details">
-        <img src={profile} alt={`${username}'s profile`} className="profile" />
-        <div className="info">
-          <h3>{username}</h3>
-          <p>{location.city}, {location.country}</p>
+    <div className="photo-list__item">
+      <img src={imageSource} alt={`${username}'s photo`} className="photo-list__image" />
+      
+      <div className="photo-list__user-details">
+        <img src={profile} alt={`${username}'s profile`} className="photo-list__user-profile" />
+        
+        <div className="photo-list__user-info">
+          <h3 className="photo-list__user-name">{username}</h3>
+          <p className="photo-list__location">{location.city}, {location.country}</p>
         </div>
       </div>
     </div>
   );
 };
+
 
 
 export default PhotoListItem;
