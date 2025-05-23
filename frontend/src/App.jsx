dfa3ba6816d12b0ca1bae98d7dad659dec7d977b
BoxinCao <ngxmex@gmail.com>
2025-05-23 12:10:51 -0400

    Display 3 Different Photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,23 +3,68 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `/profile-1.jpg`,
-};
+// const sampleDataForPhotoListItem = {
+//   id: "1",
+//   location: {
+//     city: "Montreal",
+//     country: "Canada",
+//   },
+//   imageSource: `/Image-1-Regular.jpeg`,
+//   username: "Joe Example",
+//   profile: `/profile-1.jpg`,
+// };
+
 
 
 // Note: Rendering a single component to build components in isolation
 
-const App = () => (
-  <div className="App">
-    <PhotoListItem photo={sampleDataForPhotoListItem} />
-  </div>
-);
+// const App = () => (
+//   <div className="App">
+//     <PhotoListItem photo={sampleDataForPhotoListItem} />
+//   </div>
+// );
+
+const App = () => {
+  const photoDataArray = [
+    {
+      id: "1",
+      location: { city: "Montreal", country: "Canada" },
+      imageSource: "/Image-1-Regular.jpeg",
+      username: "Joe Example",
+      profile: "/profile-1.jpg"
+    },
+    {
+      id: "2",
+      location: { city: "Toronto", country: "Canada" },
+      imageSource: "/Image-2-Regular.jpeg",
+      username: "Some One",
+      profile: "/profile-1.jpg"
+    },
+    {
+      id: "3",
+      location: { city: "Ottawa", country: "Canada" },
+      imageSource: "/Image-3-Regular.jpeg",
+      username: "Some Two",
+      profile: "/profile-1.jpg"
+    }
+  ]
+
+  return (
+    <div className="App">
+      {photoDataArray.map((photo) => (
+        <PhotoListItem
+          key={photo.id}
+          id={photo.id}
+          imageSource={photo.imageSource}
+          username={photo.username}
+          profile={photo.profile}
+          location={photo.location}
+        />
+      ))}
+    </div>
+  );
+};
+
+
+
 export default App;
